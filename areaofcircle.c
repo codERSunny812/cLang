5a6ee96dce77e17f79a8276f8ecc3ec51369c208
Sunny <sengersunny448@gmail.com>
2022-03-22 01:57:37 +0530

    first commit

---
+++ areaofcircle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a;
+float area, perimeter;
+printf("enter the radius of the circle\n");
+scanf("%d",&a);
+printf("the area of the circle is %0.2f\n",area=3.14*a*a);
+printf("the permimeter of the circle is %0.2f\n",perimeter=2*3.14*a);
+return 0;
+
+}
